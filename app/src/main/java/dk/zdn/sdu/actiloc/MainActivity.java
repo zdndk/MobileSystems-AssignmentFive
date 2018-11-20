3ca10f0123931553a780e319a637db3550907a22
Daniel Aagren Madsen <danielaagrenmadsen@outlook.dk>
2018-11-20 10:52:05 +0100

    GUI setup + dependencies

--- app/src/main/java/dk/zdn/sdu/actiloc/MainActivity.java
+++ app/src/main/java/dk/zdn/sdu/actiloc/MainActivity.java
@@ -2,12 +2,24 @@ package dk.zdn.sdu.actiloc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView _activityTextBox;
+    private Button _activityMonitoringButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SetupViews();
+    }
+
+    private void SetupViews() {
+        _activityMonitoringButton = findViewById(R.id.activityMonitoringButton);
+        _activityTextBox = findViewById(R.id.activityTextBox);
     }
 }
